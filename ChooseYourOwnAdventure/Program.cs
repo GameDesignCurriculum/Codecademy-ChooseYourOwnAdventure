590ee1efcb7b82953032498721fdd827455358d9
pcgallant <patricia.gallant@neric.org>
2023-02-11 12:13:18 -0500

    Initial commit
    starter code only

---
+++ ChooseYourOwnAdventure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+
+            // Start by asking for the user's name:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+
+
+        }
+    }
+}
+
+
+
